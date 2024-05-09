b5a13c30c8d8fd2952b018cb04d8fd6494df21c9
daffaagustians <145314938+daffaagustians@users.noreply.github.com>
2024-05-09 17:14:46 +0700

    Menambahkan ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET halaman profil. */
+router.get('/profile', function(req, res, next) {
+  // Di sini Anda akan menambahkan logika untuk mengambil data profil pengguna
+  // dan menampilkannya di halaman profil.
+  res.render('profile', { title: 'Profil Pengguna' });
 });
 
 module.exports = router;
